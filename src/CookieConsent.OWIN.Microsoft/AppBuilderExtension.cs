0e061635558199f540a42c79db20b6bf61d34422
Hudo <hudo.hudoletnjak@paddypower.com>
2015-04-27 17:39:46 +0100

    owin started

---
+++ src/CookieConsent.OWIN.Microsoft/AppBuilderExtension.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace CookieConsent.OWIN.Microsoft
+{
+    public static class AppBuilderExtension
+    {
+        public static void UseCookieConsent(this IAppBuilder appBuilder)
+        {
+            
+        }
+    }
+}
